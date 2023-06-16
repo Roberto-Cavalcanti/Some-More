d0f29e714f7fb429cacc87a8650f007034398337
Roberto-Cavalcanti <rickfreitas2001@gmail.com>
2023-06-16 00:38:32 -0300

    Telas criadas

--- app/src/main/java/com/example/somemore/MainActivity.kt
+++ app/src/main/java/com/example/somemore/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.somemore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.somemore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val btnSomar = binding.btnSomar
+    val editTextNumber = binding.editTextNumber
+    val editTextNumber2 = binding.editTextNumber2
+
+//    btnSomar.setOnClickListener{
+//
+//    }
+
+    private fun somar(n1: Float, n2:Float): Float{
+        var resultado = n1 + n2;
+        return resultado;
+    }
+
 }
