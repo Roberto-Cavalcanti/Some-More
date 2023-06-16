b5cd153d77f2bc2d4cfeee29e946b6bdf60c98d2
Roberto-Cavalcanti <rickfreitas2001@gmail.com>
2023-06-16 00:43:32 -0300

    Correção de bug

--- app/src/main/java/com/example/somemore/MainActivity.kt
+++ app/src/main/java/com/example/somemore/MainActivity.kt
@@ -2,18 +2,18 @@ package com.example.somemore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.somemore.databinding.ActivityMainBinding
+//import com.example.somemore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    val btnSomar = binding.btnSomar
-    val editTextNumber = binding.editTextNumber
-    val editTextNumber2 = binding.editTextNumber2
+//    val btnSomar = binding.btnSomar
+//    val editTextNumber = binding.editTextNumber
+//    val editTextNumber2 = binding.editTextNumber2
 
 //    btnSomar.setOnClickListener{
 //
