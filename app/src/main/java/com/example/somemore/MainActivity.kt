cf053da8c02b49a553129aee60f53644f1f078f2
Roberto-Cavalcanti <rickfreitas2001@gmail.com>
2023-06-23 00:54:21 -0300

    Soma implementada

--- app/src/main/java/com/example/somemore/MainActivity.kt
+++ app/src/main/java/com/example/somemore/MainActivity.kt
@@ -1,27 +1,33 @@
 package com.example.somemore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import com.example.somemore.databinding.ActivityMainBinding
+import com.example.somemore.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSomar.setOnClickListener { somar() }
     }
 
-//    val btnSomar = binding.btnSomar
-//    val editTextNumber = binding.editTextNumber
-//    val editTextNumber2 = binding.editTextNumber2
-
-//    btnSomar.setOnClickListener{
-//
-//    }
-
-    private fun somar(n1: Float, n2:Float): Float{
-        var resultado = n1 + n2;
-        return resultado;
+    private fun somar(){
+        var n1 = binding.editTextNumber.text.toString().toFloat()
+        var n2 = binding.editTextNumber2.text.toString().toFloat()
+        val intent = Intent(this, ResultScreen::class.java)
+        var soma = n1 + n2;
+        var resultado = "$soma";
+        intent.putExtra("result", resultado)
+//        intent.putExtra("result", resultado);
+        startActivity(intent)
     }
 
 }
+
+
--- app/src/main/java/com/example/somemore/ResultScreen.kt
+++ app/src/main/java/com/example/somemore/ResultScreen.kt
@@ -2,10 +2,21 @@ package com.example.somemore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.somemore.databinding.ActivityResultScreenBinding
 
 class ResultScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result_screen)
+        binding = ActivityResultScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        printResult()
     }
+
+    private fun printResult(){
+        val resultado = intent.getStringExtra("result")
+        binding.resultSoma.setText(resultado)
+    }
+
 }
